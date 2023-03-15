1eccde3a4bea5b54cf0133a6044f9e9c7e6d4321
helbing <helbingxxx@gmail.com>
2023-03-15 20:22:53 +0800

    chore: backup

--- docs/astro.config.ts
+++
@@ -1,7 +0,0 @@
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-import { defineConfig } from "astro/config"
-
-export default defineConfig({
-  integrations: [react(), tailwind()],
-})
