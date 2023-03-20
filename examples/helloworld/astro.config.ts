876960971026b97f9a3506f344d66f096c8e5023
helbing <helbingxxx@gmail.com>
2023-03-21 00:02:36 +0800

    chore: update

--- examples/helloworld/astro.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@astrojs/react"
-import aws from "@astrojs-aws/adapter"
-import { defineConfig } from "astro/config"
-
-export default defineConfig({
-  output: "server",
-  adapter: aws(),
-  integrations: [react()],
-})
