6210d9f8b441401c8257489b98e95d17587a99db
helbing <helbingxxx@gmail.com>
2023-03-22 13:36:13 +0800

    chore: update

--- packages/constructs/lib/lambda.ts
+++ packages/constructs/lib/lambda.ts
@@ -68,23 +68,3 @@ export class LambdaAstroSite extends Construct {
     })
   }
 }
-
-/**
- * Transform runtime string to Rumtime
- *
- * @param s
- * @returns
- */
-export function stringToRuntime(s: string): Runtime {
-  switch (s) {
-    case "nodejs14.x": {
-      return Runtime.NODEJS_14_X
-    }
-    case "nodejs16.x": {
-      return Runtime.NODEJS_16_X
-    }
-    default: {
-      return Runtime.NODEJS_18_X
-    }
-  }
-}
