b8b256a79b2b4444a49eba0accdc6a20ba2482a3
helbing <helbingxxx@gmail.com>
2023-03-22 23:19:49 +0800

    chore: update

--- packages/constructs/examples/helloworld/astro.config.ts
+++ packages/constructs/examples/helloworld/astro.config.ts
@@ -1,9 +1,10 @@
 import react from "@astrojs/react"
+import tailwind from "@astrojs/tailwind"
 import aws from "@astrojs-aws/adapter"
 import { defineConfig } from "astro/config"
 
 export default defineConfig({
   output: "server",
   adapter: aws(),
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 })
