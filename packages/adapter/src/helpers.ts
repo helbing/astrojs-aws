a041c6a97d99a6ee53e165f437ddd20b10782716
helbing <helbingxxx@gmail.com>
2023-04-07 21:54:54 +0800

    chore: update

--- packages/adapter/src/helpers.ts
+++ packages/adapter/src/helpers.ts
@@ -1,2 +1,10 @@
+/**
+ * Parse and get content-type value
+ * e.g. text/html => text/html
+ * e.g. text/html; charset=utf-8 => text/html
+ *
+ * @param header
+ * @returns
+ */
 export const parseContentType = (header?: string | null) =>
   header?.split(";")[0] ?? ""
